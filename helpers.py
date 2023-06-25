1ea2e3cb4b92ca6f556e4c7464bd5c5e489a9b8e
realhardik18 <realhardik18@gmail.com>
2023-06-26 00:46:03 +0530

    working on image retrival

--- helpers.py
+++ helpers.py
@@ -1,6 +1,20 @@
 from gtts import gTTS 
+import urllib.request 
+import wikipedia as wp
+from bs4 import BeautifulSoup
+from creds import api_key
+from google_images_download import google_images_download
 
 def GenerateAudio(text):
     speech = gTTS(text = text, tld="co.uk",lang='en-gb',slow = False)
     speech.save("file.mp3")
 
+def GetSummarizedText(topic):
+    #url=f'https://simple.wikipedia.org/wiki/{topic}'
+    #html = urllib.request.urlopen(url)
+    #htmlParse = BeautifulSoup(html, 'html.parser')
+    return wp.summary(topic, sentences = 5)
+
+#def downloadimages(query):
+
+#print(GetSummarizedText('programming'))
